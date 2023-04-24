6fb705c0c43a2f83939fa1c2f28949c99de51dda
Kosta <dukov423@mail.ru>
2023-04-24 22:29:48 +0500

    add tast.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,41 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discr = b ** 2 - 4 * a * c;
+	if (discr < 0) {
+		return arr;
+	} else if (discr === 0) {
+		let root = -b / (2 * a)
+		arr.push(root);
+		return arr;
+	} else {
+		let root = (-b + Math.sqrt(discr)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discr)) / (2 * a);
+		arr.push(root, root2)
+		return arr;
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	if (typeof percent === 'string') {
+		percent = parseInt(percent);
+	}
+	if (typeof contribution === 'string') {
+		contribution = parseInt(contribution);
+	}
+	if (typeof amount === 'string') {
+		amount = parseInt(amount);
+	}
+	if (typeof countMonths === 'string') {
+		countMonths = parseInt(countMonths);
+	}
+	if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+		return false;
+	}
+	percent = percent / 100;
+	let bodyCredit = amount - contribution;
+	let monthPercent = percent / 12;
+	let monthlyPayment = bodyCredit * (monthPercent + monthPercent / (Math.pow(1 + monthPercent, countMonths) - 1));
+	let totalAmount = monthlyPayment * countMonths;
+	return parseFloat(totalAmount.toFixed(2));
 }
