4280e9652bd10e425de1438e2ef398fd3ae7aae8
Kosta <dukov423@mail.ru>
2023-05-05 22:39:52 +0500

    add task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false
+	}
+	return arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(user => user.gender === gender).reduce((acc, curret, index, arr) => {
+    acc += curret.age;
+    if(index === arr.length - 1) {
+      return acc / arr.length;
+    }
+    return acc;
+  }, 0)
+  return result;
 }
