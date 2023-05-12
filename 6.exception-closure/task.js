4c5e8c1e62f183d90ae68f334d4ed0c3d2ef401d
Kosta <dukov423@mail.ru>
2023-05-12 22:18:16 +0500

    add task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(item) {
+  const result = Number.parseFloat(item);
+  if (isNaN(item)) {
+    throw new Error("Невалидное значение");
+  }
+  return result;
+}
+
+function validateCount(item) {
+  try {
+    return parseCount(item);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = parseCount(a);
+    this.b = parseCount(b);
+    this.c = parseCount(c);
+    if (this.a + this.b < this.c || this.a + this.c < this.b || this.b + this.c < this.a) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    const p = this.perimeter / 2;
+    return +(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    const triangle = new Triangle(a, b, c);
+    return triangle;
+  } catch (error) {
+    return {
+      get area() {
+        return 'Ошибка! Треугольник не существует';
+      },
+      get perimeter() {
+        return 'Ошибка! Треугольник не существует';
+      },
+    };
+  }
+}
