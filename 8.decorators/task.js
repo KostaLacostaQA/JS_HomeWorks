637f5c59c6e148d858c9f9702ac0e02686dc5f62
Kosta <dukov423@mail.ru>
2023-05-25 22:14:04 +0500

    add task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,45 +1,45 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-	let cache = [];
+  let cache = [];
 
-	function wrapper(...args) {
-		const hash = md5(args);
-		let objectInCache = cache.find((item) => item.hash === hash);
-		if (objectInCache) {
-			return "Из кэша: " + objectInCache.result;
-		}
+  function wrapper(...args) {
+    const hash = md5(args);
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      return "Из кэша: " + objectInCache.result;
+    }
 
-		let result = func(...args);
-		cache.push({ hash, result });
-		if (cache.length > 5) {
-			cache.shift();
-		}
-		return "Вычисляем: " + result;
-	}
-	return wrapper;
+    let result = func(...args);
+    cache.push({ hash, result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-	let timeoutId = null;
-	wrapper.count = 0;
-	wrapper.allCount = 0;
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
 
-	function wrapper(...args) {
-		if (timeoutId === null) {
-			func(args);
-			wrapper.count++;
-		}
+  function wrapper(...args) {
+    if (timeoutId === null) {
+      func(args);
+      wrapper.count++;
+    }
 
-		if (timeoutId) {
-			clearTimeout(timeoutId);
-		}
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
 
-		timeoutId = setTimeout(() => {
-			func(args);
-			wrapper.count++;
-		}, delay);
-		wrapper.allCount++;
-	}
-	return wrapper;
+    timeoutId = setTimeout(() => {
+      func(args);
+      wrapper.count++;
+    }, delay);
+    wrapper.allCount++;
+  }
+  return wrapper;
 }
